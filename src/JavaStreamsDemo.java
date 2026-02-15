91df8f42cbf93dab37f2819e6d58f8df1fdf05d9
Shivak007 <kshivaoffl2824@gmail.com>
2026-02-15 22:26:08 +0530

    [Shiva Kumar] Added: UC2.1 Create Stream and Iterate using forEach.

---
+++ src/JavaStreamsDemo.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaStreamsDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(12,78,23,98,7,56);
+
+        numbers.stream()
+                .forEach(System.out::println);
+    }
+}
