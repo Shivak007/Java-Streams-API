b1caf820e7206d714666a92c1ab8357885cc802d
Shivak007 <kshivaoffl2824@gmail.com>
2026-02-15 22:39:48 +0530

    [Shiva Kumar] Added: UC2.8 allMatch and anyMatch operations.

--- src/JavaStreamsDemo.java
+++ src/JavaStreamsDemo.java
@@ -6,16 +6,14 @@ public class JavaStreamsDemo {
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(12,78,23,98,7,56);
-        int sum = numbers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+        boolean allEven = numbers.stream()
+                .allMatch(n -> n % 2 == 0);
 
-        double average = numbers.stream()
-                .mapToInt(Integer::intValue)
-                .average()
-                .orElse(0.0);
+        boolean anyEven = numbers.stream()
+                .anyMatch(n -> n % 2 == 0);
 
-        System.out.println("Sum: " + sum);
-        System.out.println("Average: " + average);
+        System.out.println("All Even: " + allEven);
+        System.out.println("Any Even: " + anyEven);
+
     }
 }
