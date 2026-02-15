d25ecbe1d9f355fc9c9b61174977f571dd1cebca
Shivak007 <kshivaoffl2824@gmail.com>
2026-02-15 22:30:48 +0530

    [Shiva Kumar] Added: UC2.5 Find first even number.

--- src/JavaStreamsDemo.java
+++ src/JavaStreamsDemo.java
@@ -6,11 +6,9 @@ public class JavaStreamsDemo {
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(12,78,23,98,7,56);
-        List<Integer> evens =
-                numbers.stream()
-                        .filter(n -> n % 2 == 0)
-                        .collect(java.util.stream.Collectors.toList());
-
-        System.out.println(evens);
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .findFirst()
+                .ifPresent(System.out::println);
     }
 }
